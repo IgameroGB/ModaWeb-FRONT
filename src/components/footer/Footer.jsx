fa385b46eaf69cfd40383e5950fcfb7c82fa3080
igamerogb <igamero@goldenbeetle.es>
2023-09-27 12:36:02 +0200

    footer image

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,22 @@
+import styled from "@emotion/styled";
+
 const Footer = () => {
+  const FooterContainer = styled.footer`
+    width: 100%;
+    height: 300px;
+
+    background-color: #d1d1d1;
+    background-image: url("../../assets/images/Muro_footer.png");
+    background-image: url("https://i.postimg.cc/vHCsfj5Y/Muro-footer.png");
+
+    background-position-x: center;
+  `;
   return (
-    <footer>
+    <FooterContainer>
       <div>Footer</div>
       <div>Footer</div>
       <div>Footer</div>
-    </footer>
+    </FooterContainer>
   );
 };
 
--- src/components/header/HeaderLogo.jsx
+++ src/components/header/HeaderLogo.jsx
@@ -12,7 +12,22 @@ const LogoImage = styled.div``;
 const LogoText = styled.div`
   display: flex;
   flex-direction: column;
+
+  padding-left: 35px;
+  min-width: 140px;
+  display: flex;
+  align-items: center;
 `;
+
+const TextGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Text1 = styled.span``;
+const Text2 = styled.span``;
+const Text3 = styled.span``;
+const Text4 = styled.span``;
 const HeaderLogo = () => {
   return (
     <LogoContainer>
@@ -23,10 +38,26 @@ const HeaderLogo = () => {
         />
       </LogoImage> */}
       <LogoText>
-        <span>
-          Pucci <br /> di Benisichi
+        <TextGroup>
+          <div style={{ display: "flex" }}>
+            <span
+              style={{
+                fontSize: "35px",
+                lineHeight: "32px",
+                fontFamily: "var(--stylographic-font)",
+              }}
+            >
+              P
+            </span>
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              <span style={{ paddingLeft: "6px" }}>ucci</span>
+              <span>di Benisichi</span>
+            </div>
+          </div>
+        </TextGroup>
+        <span style={{ width: "100%", textAlign: "center", fontSize: "14px" }}>
+          made in Italy
         </span>
-        <span>made in Italy</span>
       </LogoText>
     </LogoContainer>
   );
