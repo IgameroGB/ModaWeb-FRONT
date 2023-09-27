62e4f8ac9405f1c9ab67ce1ce2c2a13592587152
igamerogb <igamero@goldenbeetle.es>
2023-09-27 10:24:41 +0200

    files

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,20 @@
 const Footer = () => {
   return (
-    <footer>
-      <div>Footer</div>
-      <div>Footer</div>
-      <div>Footer</div>
-    </footer>
+    <form name="contact" netlify>
+      <p>
+        <label>
+          Name <input type="text" name="name" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Email <input type="email" name="email" />
+        </label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
   );
 };
 
